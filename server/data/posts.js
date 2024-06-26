5ff024090367000cb3e526c6dd4b45ab4e1cd219
Oleksandr Yanov <oleksandr.yanov.eu@gmail.com>
2024-06-26 21:48:45 +0200

    Fix server

--- server/data/posts.js
+++ server/data/posts.js
@@ -1,14 +1,33 @@
-const fs = require('node:fs/promises');
+const fs = require('fs');
+
+// Check if posts.json exists, if not, create it with an empty array.
+const dbName = 'posts.json';
+const exists = fs.existsSync(dbName);
+
+if (!exists) {
+	fs.writeFile(
+		dbName,
+		JSON.stringify({ posts: [] }),
+		(err) => err && console.error(err)
+	);
+}
 
 async function getStoredPosts() {
-  const rawFileContent = await fs.readFile('posts.json', { encoding: 'utf-8' });
-  const data = JSON.parse(rawFileContent);
-  const storedPosts = data.posts ?? [];
-  return storedPosts;
+	const rawFileContent = await fs.promises.readFile(dbName, {
+		encoding: 'utf-8',
+	});
+	const data = JSON.parse(rawFileContent);
+	const storedPosts = data.posts ?? [];
+
+	return storedPosts;
 }
 
 function storePosts(posts) {
-  return fs.writeFile('posts.json', JSON.stringify({ posts: posts || [] }));
+	return fs.writeFile(
+		dbName,
+		JSON.stringify({ posts: posts || [] }),
+		(err) => err && console.error(err)
+	);
 }
 
 exports.getStoredPosts = getStoredPosts;
